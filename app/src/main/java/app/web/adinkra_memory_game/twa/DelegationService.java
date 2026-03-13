61e21b8e9c6d6a6c39c90c38b9a2c88ac4eee0d6
winfred <winfredgyebi@gmail.com>
2026-03-13 11:47:07 -0400

    updated with instructions

---
+++ app/src/main/java/app/web/adinkra_memory_game/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.adinkra_memory_game.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
